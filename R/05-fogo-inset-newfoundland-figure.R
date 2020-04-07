f8c356ad8c1a42db9d87c8deddadf461ba467787
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-06 21:48:00 -0300

    fst fogo inset fig

---
+++ R/05-fogo-inset-newfoundland-figure.R
@@ -0,0 +1,47 @@
+### Fogo Inset in NL Study Area Figure ====
+# Alec L. Robitaille
+
+
+### Packages ----
+libs <- c(
+	'data.table',
+	'ggplot2',
+	'sf'
+)
+lapply(libs, require, character.only = TRUE)
+
+
+### Data ----
+fogo <- readRDS('output/fogo-island-polygons.Rds')
+nl <- readRDS('output/newfoundland-polygons.Rds')
+
+# CRS
+utm <- st_crs('+proj=utm +zone=21 ellps=WGS84')
+
+
+### Theme ----
+# Colors
+watercol <- '#c3e2ec'
+islandcol <- '#d0c2a9'
+
+# Theme
+themeMap <- theme(panel.border = element_rect(size = 1, fill = NA),
+									panel.background = element_rect(fill = watercol),
+									panel.grid = element_line(color = 'black', size = 0.2),
+									axis.text = element_text(size = 11, color = 'black'),
+									axis.title = element_blank())
+
+### Plot ----
+# Base islands
+(gfogo <- ggplot(fogo) +
+ 	geom_sf(fill = islandcol) +
+ 	themeMap)
+
+
+### Output ----
+ggsave(
+	'graphics/01-fogo-island.png',
+	width = 5,
+	height = 5,
+	dpi = 320
+)
