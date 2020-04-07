f18cee05571be31dc204bac580aadd87312d0d1c
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-06 21:36:15 -0300

    fst newfoundland figure

---
+++ R/04-newfoundland-figure.R
@@ -0,0 +1,46 @@
+### Newfoundland Study Area Figure ====
+# Alec L. Robitaille
+
+
+### Packages ----
+libs <- c(
+	'data.table',
+	'ggplot2',
+	'sf'
+)
+lapply(libs, require, character.only = TRUE)
+
+
+### Data ----
+nl <- readRDS('output/newfoundland-polygons.Rds')
+
+# CRS
+utm <- st_crs('+proj=utm +zone=21 ellps=WGS84')
+
+
+### Theme ----
+# Colors
+watercol <- '#9ccfe0'
+islandcol <- '#d0c2a9'
+
+# Theme
+themeMap <- theme(panel.border = element_rect(size = 1, fill = NA),
+									panel.background = element_rect(fill = ),
+									panel.grid = element_line(color = 'black', size = 0.2),
+									axis.text = element_text(size = 11, color = 'black'),
+									axis.title = element_blank())
+
+### Plot ----
+# Base NL
+(gfogo <- ggplot(nl) +
+ 	geom_sf(fill = islandcol) +
+ 	themeMap)
+
+
+### Output ----
+ggsave(
+	'graphics/01-newfoundland.png',
+	width = 5,
+	height = 5,
+	dpi = 320
+)
