753614ee184e3fe659fbc498e8106cefe0f58102
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-15 19:27:20 -0300

    add primary roads to tn buns

--- R/08-terra-nova-buns-figure.R
+++ R/08-terra-nova-buns-figure.R
@@ -45,7 +45,8 @@ water <- st_read('output/terra-nova-water.gpkg')
 utm <- st_crs('+proj=utm +zone=21 ellps=WGS84')
 
 # Only main highway
-highway <- roads[roads$highway == 'trunk',]
+selroads <- c('trunk', 'primary')
+highway <- roads[roads$highway %in% selroads,]
 
 
 ### Theme ----
@@ -59,6 +60,10 @@ roadcol <- '#191919'
 
 parkcol <- '#9fb5a0'
 parkboundcol <- '#4c5d3a'
+
+roadcols <- data.table(highway = selroads)
+roadcols[, cols := gray.colors(.N, start = 0.1, end = 0.4)]
+roadpal <- roadcols[, setNames(cols, highway)]
 
 
 # Theme
@@ -80,42 +85,43 @@ nlcrop <- st_crop(nl, bbadjust + rep(c(-5e4, 5e4), each = 2))
 
 # Base NL with red box indicating TN
 (gnl <- ggplot() +
-	geom_sf(fill = islandcol, size = 0.3, color = coastcol, data = nl) +
-	geom_sf(fill = parkcol, size = 0.3, color = coastcol, data = tn) +
-	geom_rect(
-		aes(
-			xmin = bb['xmin'],
-			xmax = bb['xmax'],
-			ymin = bb['ymin'],
-			ymax = bb['ymax']
-		),
-		fill = NA,
-		size = 1.5,
-		color = 'red'
-	) +
- 	themeMap +
-	theme(axis.text = element_blank(),
-				axis.ticks = element_blank(),
-				plot.margin = grid::unit(c(-1,-1,-1,-1), "mm")))
+		geom_sf(fill = islandcol, size = 0.3, color = coastcol, data = nl) +
+		geom_sf(fill = parkcol, size = 0.3, color = coastcol, data = tn) +
+		geom_rect(
+			aes(
+				xmin = bb['xmin'],
+				xmax = bb['xmax'],
+				ymin = bb['ymin'],
+				ymax = bb['ymax']
+			),
+			fill = NA,
+			size = 1.5,
+			color = 'red'
+		) +
+		themeMap +
+		theme(axis.text = element_blank(),
+					axis.ticks = element_blank(),
+					plot.margin = grid::unit(c(-1,-1,-1,-1), "mm")))
 
 # Base terra-nova
 (gtn <- ggplot() +
- 	geom_sf(fill = islandcol, size = 0.3, color = coastcol, data = nlcrop) +
- 	geom_sf(fill = parkcol, size = 0.3, color = parkboundcol, data = tn) +
-	geom_sf(fill = watercol, size = 0.2, color = coastcol, data = water) +
- 	geom_sf(color = roadcol, data = highway) +
- 	geom_point(aes(x, y), data = grids) +
- 	geom_label_repel(aes(x, y, label = SiteName), data = grids) +
- 	coord_sf(xlim = c(bbadjust['xmin'], bbadjust['xmax']),
- 					 ylim = c(bbadjust['ymin'], bbadjust['ymax'])) +
- 	guides(color = FALSE) +
- 	themeMap)
+		geom_sf(fill = islandcol, size = 0.3, color = coastcol, data = nlcrop) +
+		geom_sf(fill = parkcol, size = 0.3, color = parkboundcol, data = tn) +
+		geom_sf(fill = watercol, size = 0.2, color = coastcol, data = water) +
+		geom_sf(aes(color = highway), data = highway) +
+		scale_color_manual(values = roadpal) +
+		geom_point(aes(x, y), data = grids) +
+		geom_label_repel(aes(x, y, label = SiteName), data = grids) +
+		coord_sf(xlim = c(bbadjust['xmin'], bbadjust['xmax']),
+						 ylim = c(bbadjust['ymin'], bbadjust['ymax'])) +
+		guides(color = FALSE) +
+		themeMap)
 
 # Use annotation custom to drop the NL inset on Fogo
 # Numbers here taken from mapview(tn), just reading off the map
 annotateSf <- st_sfc(st_multipoint(matrix(c(-53.8, -53.65,
 																						48.35, 48.45),
-																 nrow = 2)))
+																					nrow = 2)))
 st_crs(annotateSf) <- 4326
 annotateBB <- st_bbox(st_transform(annotateSf, utm))
 
