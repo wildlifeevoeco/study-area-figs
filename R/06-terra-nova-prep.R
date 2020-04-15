685533ceb2b98ee78df5e420e715133da1fe8b11
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-15 14:13:19 -0300

    start roads

--- R/06-terra-nova-prep.R
+++ R/06-terra-nova-prep.R
@@ -7,8 +7,8 @@ libs <- c('curl', 'zip', 'sf')
 lapply(libs, require, character.only = TRUE)
 
 
-### Download Terra Nova polygon ----
-# From Open Canada
+### Download Terra Nova data ----
+## Polygon from Open Canada
 # https://open.canada.ca/data/en/dataset/e1f0c975-f40c-4313-9be2-beb951e35f4e
 curl_download('http://ftp.maps.canada.ca/pub/pc_pc/National-parks_Parc-national/national_parks_boundaries/national_parks_boundaries.shp.zip', 'input/national-parks.zip')
 
@@ -17,6 +17,17 @@ unzip('input/national-parks.zip', exdir = 'input/national-parks')
 parks <- st_read('input/national-parks')
 
 tn <- parks[parks$parkname_e == 'Terra Nova National Park of Canada', ]
+
+
+## Roads
+st_bbox(st_buffer(tn, 1e6))
+routes <- opq(bb) %>%
+	add_osm_feature(key = 'highway') %>%
+	osmdata_sf()
+
+# Grab roads
+roads <- routes$osm_lines
+
 
 
 ### Reproject ----
