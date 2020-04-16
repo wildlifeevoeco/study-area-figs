2e86b4a5feee16b4b17ef5b85fb8580daa264b2f
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-16 19:37:03 -0300

    fst palette script

---
+++ R/00-palette.R
@@ -0,0 +1,11 @@
+### Palette - Study area figure ====
+# Alec Robitaille
+
+watercol <- '#c3e2ec'
+islandcol <- '#d0c2a9'
+coastcol <- '#82796a'
+roadcol <- '#666666'
+gridcol <- '#323232'
+
+parkcol <- '#9fb5a0'
+parkboundcol <- '#4c5d3a'
