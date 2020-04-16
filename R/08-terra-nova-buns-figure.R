6064ec7e7560f2895311deafd5c2ae1a1aa136fc
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-16 19:32:32 -0300

    use stream col for coast

--- R/08-terra-nova-buns-figure.R
+++ R/08-terra-nova-buns-figure.R
@@ -52,8 +52,10 @@ highway <- roads[roads$highway %in% selroads,]
 ### Theme ----
 # Colors
 watercol <- '#c3e2ec'
+streamcol <- '#9cb4bc'
+coastcol <- streamcol
+
 islandcol <- '#d0c2a9'
-coastcol <- '#82796a'
 roadcol <- '#666666'
 gridcol <- '#323232'
 
