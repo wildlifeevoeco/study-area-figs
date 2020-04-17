38f2b57ddad5d685aaea711eee840b9c077c6255
Juliana Balluffi-Fry <jballuffi@gmail.com>
2020-04-16 23:15:06 -0230

    always create input dir

--- R/06-terra-nova-prep.R
+++ R/06-terra-nova-prep.R
@@ -11,7 +11,7 @@ lapply(libs, require, character.only = TRUE)
 ## Polygon from Open Canada
 # https://open.canada.ca/data/en/dataset/e1f0c975-f40c-4313-9be2-beb951e35f4e
 curl_download('http://ftp.maps.canada.ca/pub/pc_pc/National-parks_Parc-national/national_parks_boundaries/national_parks_boundaries.shp.zip', 'input/national-parks.zip')
-
+dir.create('input')
 unzip('input/national-parks.zip', exdir = 'input/national-parks')
 
 parks <- st_read('input/national-parks')
