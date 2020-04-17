54053dc3a8dc2f56f9bd13ae423c096f38b74aa2
Juliana Balluffi-Fry <jballuffi@gmail.com>
2020-04-16 23:14:55 -0230

    add stream col

--- R/00-palette.R
+++ R/00-palette.R
@@ -3,9 +3,10 @@
 
 watercol <- '#c3e2ec'
 islandcol <- '#d0c2a9'
-coastcol <- '#82796a'
 roadcol <- '#666666'
 gridcol <- '#323232'
+streamcol <- '#9cb4bc'
+coastcol <- streamcol
 
 parkcol <- '#9fb5a0'
 parkboundcol <- '#4c5d3a'
