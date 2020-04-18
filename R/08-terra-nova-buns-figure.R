ccc0e4c8ffe2f9852cdce9a46a51a9f85219320f
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-18 11:18:52 -0300

    roads above streams

--- R/08-terra-nova-buns-figure.R
+++ R/08-terra-nova-buns-figure.R
@@ -102,10 +102,10 @@ nlcrop <- st_crop(nl, bbadjust + rep(c(-5e4, 5e4), each = 2))
 		geom_sf(fill = islandcol, size = 0.3, color = coastcol, data = nlcrop) +
 		geom_sf(fill = parkcol, size = 0.3, color = parkboundcol, data = tn) +
 		geom_sf(fill = watercol, size = 0.2, color = coastcol, data = water) +
-		geom_sf(aes(color = highway), data = highway) +
 		scale_color_manual(values = roadpal) +
 		geom_sf(fill = streamcol, color = NA, data = streamPols) +
 		geom_sf(color = streamcol, size = 0.4, data = streamLns) +
+		geom_sf(aes(color = highway), data = highway) +
 		geom_point(aes(x, y), data = grids) +
 		geom_sf_label(aes(label = 'Terra Nova National Park'), data = tn, fill = '#bbcbbc') +
 		geom_label_repel(aes(x, y, label = SiteName), size = 4.5, data = grids) +
