f5e2901e0a8da7d3ab1227774335fdc6a9e10756
Alec L. Robitaille <robit.alec@gmail.com>
2020-04-18 11:12:57 -0300

    org palette colors

--- R/00-palette.R
+++ R/00-palette.R
@@ -1,13 +1,19 @@
 ### Palette - Study area figure ====
 # Alec Robitaille
 
+# Water
 watercol <- '#c3e2ec'
-islandcol <- '#d0c2a9'
-roadcol <- '#666666'
-gridcol <- '#323232'
 streamcol <- '#9cb4bc'
 coastcol <- streamcol
+
+# Land
+islandcol <- '#d0c2a9'
+
+# Anthro
+roadcol <- '#666666'
 
 parkcol <- '#9fb5a0'
 parkboundcol <- '#4c5d3a'
 
+# Map etc
+gridcol <- '#323232'
