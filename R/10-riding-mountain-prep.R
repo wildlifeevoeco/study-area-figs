34d17e775f752aff846b4e89997a28f7e3af0ecc
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-06 19:07:21 -0300

    fst riding mountain prep

---
+++ R/10-riding-mountain-prep.R
@@ -0,0 +1,35 @@
+### Riding Mountain - Prep ====
+# Alec L. Robitaille
+
+
+### Packages ----
+libs <- c('sf', 'osmdata')
+lapply(libs, require, character.only = TRUE)
+
+### Download OSM data ----
+# Download osm coastlines in bbox
+# NOTE: This steps takes a few moments
+zz <- opq(getbb('Riding Mountain National Park ')) %>%
+	add_osm_feature(key = 'boundary', value = 'national_park') %>%
+	osmdata_sf()
+#
+# # Grab lines
+# lns <- zz$osm_lines
+#
+# # Union -> polygonize -> cast lines = geometry set
+# castpolys <- st_cast(st_polygonize(st_union(lns)))
+#
+# # Combine geometries and cast as sf
+# nl <- st_as_sf(castpolys)
+#
+#
+# ### Reproject islands ----
+# # Projections
+# utm <- st_crs('+proj=utm +zone=21 ellps=WGS84')
+#
+# # Project to UTM
+# utmNL <- st_transform(nl, utm)
+#
+#
+# ### Output ----
+# st_write(utmNL, 'output/newfoundland-polygons.gpkg')
