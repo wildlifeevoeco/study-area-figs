42580d2bdb999d4983c046614718daa32fef181f
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-06 20:21:14 -0300

    resolve combine water

--- R/10-riding-mountain-prep.R
+++ R/10-riding-mountain-prep.R
@@ -10,7 +10,7 @@ lapply(libs, require, character.only = TRUE)
 library(mapview)
 
 # Bounding box (min xy, max xy)
-bb <- c(-101.1758, 50.6216, -99.4016, 51.1811)
+bb <- c(-101.1758, 50.3244, -99.4016, 51.1811)
 
 # Download RMNP bounds
 boundscall <- opq(bb) %>%
@@ -25,31 +25,17 @@ water <- opq(bb) %>%
 	osmdata_sf()
 
 waterlns <- st_combine(water$osm_lines)
-waterpols <- st_combine(water$osm_polygons)
-watermpolys <- st_combine(water$osm_multipolygons)
+polys <- st_buffer(st_combine(water$osm_polygons), 0)
+mpolys <- st_combine(water$osm_multipolygons)
 
-rbind(waterpolys, watermpolys)
+waterpolys <- st_union(polys, mpolys)
 
-forest <- opq(bb) %>%
+plot(waterpolys)
+
 	add_osm_feature(key = 'natural', value = c('forest', 'wood')) %>%
 	osmdata_sf()
 
-waterlns[which(colnames(waterlns) %in% c('osm_id', 'geometry')),]
-colnames(waterlns)
 
-plot(waterlns)
-plot(waterpols)
-
-# # Grab lines
-# lns <- zz$osm_lines
-#
-# # Union -> polygonize -> cast lines = geometry set
-# castpolys <- st_cast(st_polygonize(st_union(lns)))
-#
-# # Combine geometries and cast as sf
-# nl <- st_as_sf(castpolys)
-#
-#
 # ### Reproject islands ----
 # # Projections
 # utm <- st_crs('+proj=utm +zone=21 ellps=WGS84')
