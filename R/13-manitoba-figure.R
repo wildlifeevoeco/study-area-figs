acdb1d6056828b2f63ee23a66e9b155da9b7e750
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-20 18:56:10 -0230

    fst manitoba fig

---
+++ R/13-manitoba-figure.R
@@ -0,0 +1,13 @@
+# === Manitoba - Figure ------------------------------
+# Alec L. Robitaille
+
+
+# Packages ----------------------------------------------------------------
+libs <- c(
+	'data.table',
+	'ggplot2',
+	'sf'
+)
+lapply(libs, require, character.only = TRUE)
+
+
