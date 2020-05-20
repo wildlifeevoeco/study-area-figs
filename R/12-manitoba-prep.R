6aefcacbc341ed33c98ef97312a313b288ead0c8
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-20 18:06:28 -0230

    get bounds from natural earth

--- R/12-manitoba-prep.R
+++ R/12-manitoba-prep.R
@@ -9,6 +9,20 @@ lapply(libs, require, character.only = TRUE)
 
 
 # Download OSM data -------------------------------------------------------
+bounds <- ne_download(
+	scale = 'large',
+	type = 'states',
+	category = 'cultural',
+	returnclass = 'sf'
+)
+
+admin only
+
+zz <- opq(getbb('Newfoundland')) %>%
+	add_osm_feature(key = 'place', value = 'island') %>%
+	osmdata_sf()
+
+get borders of all provinces and usa
 
 # Prep geometries ---------------------------------------------------------
 
