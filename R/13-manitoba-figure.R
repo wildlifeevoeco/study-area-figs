9dc0fcbe6ed55471bc5ccaf00d76d8b45156d451
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-20 18:59:24 -0230

    add data, colors, theme

--- R/13-manitoba-figure.R
+++ R/13-manitoba-figure.R
@@ -11,3 +11,30 @@ libs <- c(
 lapply(libs, require, character.only = TRUE)
 
 
+
+# Data --------------------------------------------------------------------
+bounds <- st_read('output/manitoba-bounds.gpkg')
+lakes <- st_read('output/manitoba-lakes.gpkg')
+
+
+# CRS
+utm <- st_crs(4326)
+
+
+# Theme -------------------------------------------------------------------
+## Colors
+source('R/00-palette.R')
+
+## Theme
+themeMap <- theme(panel.border = element_rect(size = 1, fill = NA),
+									panel.background = element_rect(fill = landcol),
+									panel.grid = element_line(color = gridcol, size = 0.3),
+									axis.text = element_text(size = 11, color = 'black'),
+									axis.title = element_blank())
+
+
+# Plot --------------------------------------------------------------------
+# Base rmnp
+mb <- bounds[bounds$name_en == 'Manitoba',]
+
+bb <- st_bbox(st_buffer(st_centroid(mb), 1e9))
