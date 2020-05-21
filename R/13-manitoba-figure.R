15db4ddbab7e0667fbbdc2415a78a984c0d17fa5
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-21 15:26:14 -0230

    use watercol as background

--- R/13-manitoba-figure.R
+++ R/13-manitoba-figure.R
@@ -27,7 +27,7 @@ source('R/00-palette.R')
 
 ## Theme
 themeMap <- theme(panel.border = element_rect(size = 1, fill = NA),
-									panel.background = element_rect(fill = landcol),
+									panel.background = element_rect(fill = watercol),
 									panel.grid = element_line(color = gridcol, size = 0.3),
 									axis.text = element_text(size = 11, color = 'black'),
 									axis.title = element_blank())
