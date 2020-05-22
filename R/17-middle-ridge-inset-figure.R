92c8d51cb205698e0fb32841e76ff49a35aae769
Alec L. Robitaille <robit.alec@gmail.com>
2020-05-22 09:35:31 -0230

    fst inset middle ridge

---
+++ R/17-middle-ridge-inset-figure.R
@@ -0,0 +1,85 @@
+# === Middle Ridge - Inset Figure ------------------------------
+# Alec L. Robitaille
+
+
+# Packages ----------------------------------------------------------------
+libs <- c(
+	'data.table',
+	'ggplot2',
+	'sf'
+)
+lapply(libs, require, character.only = TRUE)
+
+
+
+# Data --------------------------------------------------------------------
+bounds <- st_read('output/manitoba-bounds.gpkg')
+lakes <- st_read('output/manitoba-lakes.gpkg')
+
+
+# CRS
+crs <- st_crs('ESRI:102001')
+
+
+# Theme -------------------------------------------------------------------
+## Colors
+source('R/00-palette.R')
+
+## Theme
+themeMap <- theme(panel.border = element_rect(size = 1, fill = NA),
+									panel.background = element_rect(fill = watercol),
+									panel.grid = element_line(color = gridcol, size = 0.2),
+									axis.text = element_text(size = 11, color = 'black'),
+									axis.title = element_blank())
+
+
+# Plot --------------------------------------------------------------------
+bounds <- st_transform(bounds, crs)
+lakes <- st_transform(lakes, crs)
+
+# Base rmnp
+nl <- bounds[bounds$name_en == 'Newfoundland and Labrador',]
+
+bb <- st_bbox(st_buffer(nl, 1e3))
+
+gnl <- ggplot() +
+	geom_sf(fill = islandcol, color = 'black', size = 0.1, data = bounds) +
+	geom_sf(fill = watercol, color = streamcol, size = 0.1, data = lakes) +
+	geom_sf(fill = '#cd0001', color = NA, alpha = 0.3, data = nl) +
+	guides(color = FALSE, fill = FALSE) +
+	labs(x = NULL, y = NULL) +
+	# geom_point(aes(x, y), size = 5, shape = 18,
+	# 					 data = data.table(x = -295169, y = 1197566)) +
+	theme(axis.text = element_blank(),
+				axis.ticks = element_blank(),
+				plot.margin = grid::unit(c(-1,-1,-1,-1), "mm"))
+
+gnl
+
+# Source MR figure ----------------------------------------------
+source('R/16-middle-ridge-figure.R')
+
+
+
+
+# Combine -----------------------------------------------------------------
+# used the ggannotate package to make finding these numbers easier
+g <- grmnp +
+	annotation_custom(
+		ggplotGrob(gmbAdjust),
+		xmin = 340770,
+		ymin = 5584909,
+		xmax = 377059,
+		ymax = 5612715)
+
+
+
+
+# Output ------------------------------------------------------------------
+ggsave(
+	'graphics/14-riding-mountain-inset.png',
+	g,
+	width = 10,
+	height = 10,
+	dpi = 320
+)
