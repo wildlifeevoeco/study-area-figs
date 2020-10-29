ac58b072d03d792d2e19c91ea7a8a3aeece11e62
Quinn Webber <webber.quinn@gmail.com>
2020-10-29 16:32:06 -0230

    Update 19-Caribou-diet-North-America-prep.R

---
+++ R/19-Caribou-diet-North-America-prep.R
@@ -0,0 +1,44 @@
+
+
+
+# === North America - Prep -----------------------------------------------------
+# Alec Robitaille and Quinn M.R. Webber
+
+
+
+# Packages ----------------------------------------------------------------
+libs <- c('sf', 'osmdata', 'rnaturalearth')
+lapply(libs, require, character.only = TRUE)
+
+
+# Download OSM data -------------------------------------------------------
+## Bounds
+bounds <- ne_download(
+	scale = 'large',
+	type = 'states',
+	category = 'cultural',
+	returnclass = 'sf'
+)
+
+keepAdmin <- c('United States of America', 'Canada', 'Greenland', 'Russia', 'Iceland')
+
+keepb <- bounds[bounds$admin %in% keepAdmin,]
+
+
+## Lakes
+lakes <- rnaturalearth::ne_download(
+	scale = 'large',
+	type = 'lakes',
+	category = 'physical',
+	returnclass = 'sf'
+)
+
+wi <- st_within(lakes, keepb)
+subwi <- vapply(wi, function(x) length(x) >= 1, TRUE)
+
+keepl <- lakes[subwi, ]
+
+
+# Output ------------------------------------------------------------------
+st_write(keepl, 'output/NA-lakes.gpkg', append = F)
+st_write(keepb, 'output/NA-bounds.gpkg', append = F)
